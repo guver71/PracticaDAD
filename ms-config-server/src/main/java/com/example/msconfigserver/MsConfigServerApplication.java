abe0582b0a58fec464318082139aaf461419c266
Jarvis <jarvis@BOHK-WAX9X-634c3fa9.localdomain>
2023-09-17 23:03:04 -0500

    za 01

--- ms-config-server/src/main/java/com/example/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/example/msconfigserver/MsConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.example.msconfigserver;
 
-import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -14,4 +13,3 @@ public class MsConfigServerApplication {
 	}
 
 }
-	
